38216cea986f2b7b38b0c72856941bc372d183a7
Daniel Hollands <daniel@limeblast.co.uk>
2014-01-30 20:23:47 +0000

    I guess I didn't need to interface that method.

--- www/app/lib/Impl/Repo/Tag/TagInterface.php
+++ www/app/lib/Impl/Repo/Tag/TagInterface.php
@@ -2,15 +2,4 @@
 
 interface TagInterface {
 
-	/**
-	 * Get articles by their tag
-	 *
-	 * @param string $tag   URL slug of tag.
-	 * @param int    $page  Current page.
-	 * @param int    $limit Number of articles per page.
-	 *
-	 * @return object Object with $items and $totalItems for pagination.
-	 */
-	public function articles();
-
 }
